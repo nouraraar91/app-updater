49dccb714b0f4f2cabd27ea856555235558c58cf
DaRkViRuS2012 <nour_araar_2009@yahoo.com>
2025-04-28 02:03:02 +0800

    Initial Commit

---
+++ App Updater/App_UpdaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_UpdaterApp.swift
+//  App Updater
+//
+//  Created by Nour Araar on 28/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct App_UpdaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App UpdaterTests/App_UpdaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  App_UpdaterTests.swift
+//  App UpdaterTests
+//
+//  Created by Nour Araar on 28/04/2025.
+//
+
+import Testing
+@testable import App_Updater
+
+struct App_UpdaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
